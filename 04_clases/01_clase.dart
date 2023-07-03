57c60df3558754a06ce6185518e60ff494d2595a
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2023-07-03 11:05:16 -0500

    class in dart

---
+++ 04_clases/01_clase.dart
@@ -0,0 +1,16 @@
+import '../classes/Persona.dart';
+
+void main(List<String> args) {
+
+
+  final persona = new Persona(33, 'mauricio');
+  // persona..nombre = 'Mau'
+  //         ..edad = 23;
+          // ..bio = 'Wakanda';
+  // persona.bio = 'otro valor';
+  
+  print(persona);
+
+  
+}
+
